f38baf171c416eabe87eb9ab2369bcb6c803c284
张杰 <1700117425@qq.com>
2018-05-02 23:42:24 +0800

    test
    Test the eye recognition function of opencv.

--- data_center/data_producer.py
+++ data_center/data_producer.py
@@ -1,17 +1,17 @@
 import cv2
 
 class DataProducer:
-    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-    eyeglasses_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
+    eye_cascade = cv2.CascadeClassifier('../opencv_data/haarcascade_eye.xml')
+    eyeglasses_cascade = cv2.CascadeClassifier('../opencv_data/haarcascade_eye_tree_eyeglasses.xml')
 
     def cutEyeFromImage(self, image):
         # image: gray image
 
         # cut the eyes from gray image by the CascadeClassifier
-        self.eye_cascade.detectMultiScale(image)
+        rect = self.eye_cascade.detectMultiScale(image)
         pass
 
 if __name__ == '__main__':
-    im=cv2.imread('test_image/180.jpg')
+    im=cv2.imread('../test_image/180.jpg')
     dataProducer=DataProducer()
     dataProducer.cutEyeFromImage(im)
