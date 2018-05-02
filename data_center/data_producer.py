9efb93d33f20171ea37e4cefccda772a8aeef470
张杰 <1700117425@qq.com>
2018-05-02 22:50:40 +0800

    Initial commit

---
+++ data_center/data_producer.py
@@ -0,0 +1,19 @@
+import cv2
+
+
+class DataProducer:
+    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+    eyeglasses_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
+
+    def cutEyeFromImage(self, image):
+        # image: gray image
+
+        # cut the eyes from gray image by the CascadeClassifier
+        self.eye_cascade.detectMultiScale(image)
+
+        pass
+
+if __name__ == '__main__':
+    im=cv2.imread()
+    dataProducer=DataProducer()
+    dataProducer.cutEyeFromImage(im)
